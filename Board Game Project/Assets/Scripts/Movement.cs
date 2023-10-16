72886ddcbc24af0fe52aa27a223e5739954c0b8a
Emily <emilyperron2014@gmail.com>
2023-10-15 22:32:52 -0400

    Replaced Pause Menu With Quit Button

--- Board Game Project/Assets/Scripts/Movement.cs
+++ Board Game Project/Assets/Scripts/Movement.cs
@@ -89,128 +89,132 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!_isStarted)
-        {
-            _text.color = Color.white;
-            _text.text = "Press 'Space' to Roll the Dice. You must get a 1 to start the game.";
+       
 
-            if (Input.GetKeyDown(KeyCode.Space))
+
+            if (!_isStarted)
             {
-                _diceAmount = _die.RollDice();
-                Debug.Log("JJD");
-                if (_diceAmount == 1)
+                _text.color = Color.white;
+                _text.text = "Press 'Space' to Roll the Dice. You must get a 1 to start the game.";
+
+                if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    _text.text = "You rolled a one!";
-                    //set the flag to tru to start the game
-                    _isStarted = true;
-                    //i++;
-                    MoveOneTile();
-                    MoveOneTile();
-                    MoveOneTile();
-                    _isMoving = false;
-                }
-                else
-                {
-                    _text.text = "You didn't roll a one. Please try again.";
-                }
-            }
-
-        }
-        else if (_isStarted)
-        {
-
-
-
-
-            CheckWin(); //Runs the method CheckWin(); to see if the user has reached tile 40. 
-
-            if (!_gameIsover)
-            { //Will run when the game is not over
-                _currentPos = _players[_currentPlayer].GetPosition(); //Gets the players current position
-
-                /*Player Turn Setting*/
-
-                if (_tileMovementAmount == 0 && !_isMoving && _nextPlayerTurn == true)
-                { //When the dice is not rolled yet, and we are not moving it will run this statement. 
-                    _text.text = $" Player {_currentPlayer + 1}'s turn! Press 'Space' to roll the dice."; //Text to the players guiding keys to press to move. 
-
-                    if (_turnStarted)
-                    { //Will check for turn bool and set it to false during this statement. As nothing is being moved. 
-                        _turnStarted = false;
-
-                        if (_currentPlayer == 0)
-                        { //Player 1's turn is element 0, which is done initially before this "if" statement. After its done, _currentplayer will be set to element 1, which is Player 2. The player 2 will also go through the cycle below.   
-                            _currentPlayer = 1;
-                        }
-                        else if (_currentPlayer == 1)
-                        { //When player 2's turn is over, we move onto player 3's turn. It follows the same idea as above. 
-                            _currentPlayer = 2;
-                        }
-
-                        else if (_currentPlayer == 2)
-                        { // When player 3's turn is over, we move onto player 4's turn. It follows the same idea as above. 
-                            _currentPlayer = 3;
-                        }
-                        else
-                        {
-                            _currentPlayer = 0;  //After a cycle through all the players, the "else" statement will put the _currentPlyer back to elemnt 0, Player 1's turn. 
-                        }
+                    _diceAmount = _die.RollDice();
+                    Debug.Log("JJD");
+                    if (_diceAmount == 1)
+                    {
+                        _text.text = "You rolled a one!";
+                        //set the flag to tru to start the game
+                        _isStarted = true;
+                        //i++;
+                        MoveOneTile();
+                        MoveOneTile();
+                        MoveOneTile();
+                        _isMoving = false;
+                    }
+                    else
+                    {
+                        _text.text = "You didn't roll a one. Please try again.";
                     }
                 }
 
-                /*Player Movement When "Space" is Pressed*/
+            }
+            else if (_isStarted)
+            {
 
-                if (Input.GetKeyDown(KeyCode.Space) && _tileMovementAmount == 0 && _moveOn == true)
-                { //If the spacekey is pressed and the tilemovementamount is set to 0 (Which it should be since there is no initial die value), it will roll the dice. 
-                    _tileMovementAmount = _die.RollDice(); //Dice will be rolled from the die script and is set tothe tilemovement variable 
-                    _text.text = "You rolled a " + _tileMovementAmount.ToString() + "."; //Text to show what the user rolled converted ToString(); since its in text form. 
-                    _turnStarted = true; //When we are rolling the dice, we dont want other actions to be done, unless stated. So the turn has started 
 
-                }
 
-                /*Player Color Setting*/
 
-                if (_currentPlayer == 0)
-                { //Color green when its player 1's turn
-                    _text.color = Color.green;
-                }
-                else if (_currentPlayer == 1)
-                { //Color grey when its player 2's turn
-                    _text.color = Color.grey;
-                }
-                else if (_currentPlayer == 2)
-                { //Color red when its player 3's turn
-                    _text.color = Color.red;
-                }
-                else if (_currentPlayer == 3)
-                { //Color yellow when its player 4's turn
-                    _text.color = Color.yellow;
-                }
+                CheckWin(); //Runs the method CheckWin(); to see if the user has reached tile 40. 
 
-                /*Player Single Tile Movement*/
+                if (!_gameIsover)
+                { //Will run when the game is not over
+                    _currentPos = _players[_currentPlayer].GetPosition(); //Gets the players current position
 
-                if (_tileMovementAmount > 0 && !_isMoving)
-                { //When the rolled dice amount is greater than 0, and is not moving, we are moving one tile based on the board script. This allows to move one tile over to another. Not a fluid transformation. 
-                    MoveOneTile();
-                    _nextPlayerTurn = false;
-                }
+                    /*Player Turn Setting*/
 
-                /*Player Position Update Afrer Movement*/
+                    if (_tileMovementAmount == 0 && !_isMoving && _nextPlayerTurn == true)
+                    { //When the dice is not rolled yet, and we are not moving it will run this statement. 
+                        _text.text = $" Player {_currentPlayer + 1}'s turn! Press 'Space' to roll the dice."; //Text to the players guiding keys to press to move. 
 
-                if (_isMoving)
-                { //When the player is not moving and has finished moving from moving one tile, it will update its position from the method UpdatePosition();
-                    UpdatePostion();
-                }
+                        if (_turnStarted)
+                        { //Will check for turn bool and set it to false during this statement. As nothing is being moved. 
+                            _turnStarted = false;
 
-                //if player has done their roll and moved, run OnEventTile() function to see if they are on event tile and make event run in accordance  
-                if (_tileMovementAmount == 0 && !_isMoving && _nextPlayerTurn == false)
-                {
+                            if (_currentPlayer == 0)
+                            { //Player 1's turn is element 0, which is done initially before this "if" statement. After its done, _currentplayer will be set to element 1, which is Player 2. The player 2 will also go through the cycle below.   
+                                _currentPlayer = 1;
+                            }
+                            else if (_currentPlayer == 1)
+                            { //When player 2's turn is over, we move onto player 3's turn. It follows the same idea as above. 
+                                _currentPlayer = 2;
+                            }
 
-                    OnEventTile();
+                            else if (_currentPlayer == 2)
+                            { // When player 3's turn is over, we move onto player 4's turn. It follows the same idea as above. 
+                                _currentPlayer = 3;
+                            }
+                            else
+                            {
+                                _currentPlayer = 0;  //After a cycle through all the players, the "else" statement will put the _currentPlyer back to elemnt 0, Player 1's turn. 
+                            }
+                        }
+                    }
 
+                    /*Player Movement When "Space" is Pressed*/
+
+                    if (Input.GetKeyDown(KeyCode.Space) && _tileMovementAmount == 0 && _moveOn == true)
+                    { //If the spacekey is pressed and the tilemovementamount is set to 0 (Which it should be since there is no initial die value), it will roll the dice. 
+                        _tileMovementAmount = _die.RollDice(); //Dice will be rolled from the die script and is set tothe tilemovement variable 
+                        _text.text = "You rolled a " + _tileMovementAmount.ToString() + "."; //Text to show what the user rolled converted ToString(); since its in text form. 
+                        _turnStarted = true; //When we are rolling the dice, we dont want other actions to be done, unless stated. So the turn has started 
+
+                    }
+
+                    /*Player Color Setting*/
+
+                    if (_currentPlayer == 0)
+                    { //Color green when its player 1's turn
+                        _text.color = Color.green;
+                    }
+                    else if (_currentPlayer == 1)
+                    { //Color grey when its player 2's turn
+                        _text.color = Color.grey;
+                    }
+                    else if (_currentPlayer == 2)
+                    { //Color red when its player 3's turn
+                        _text.color = Color.red;
+                    }
+                    else if (_currentPlayer == 3)
+                    { //Color yellow when its player 4's turn
+                        _text.color = Color.yellow;
+                    }
+
+                    /*Player Single Tile Movement*/
+
+                    if (_tileMovementAmount > 0 && !_isMoving)
+                    { //When the rolled dice amount is greater than 0, and is not moving, we are moving one tile based on the board script. This allows to move one tile over to another. Not a fluid transformation. 
+                        MoveOneTile();
+                        _nextPlayerTurn = false;
+                    }
+
+                    /*Player Position Update Afrer Movement*/
+
+                    if (_isMoving)
+                    { //When the player is not moving and has finished moving from moving one tile, it will update its position from the method UpdatePosition();
+                        UpdatePostion();
+                    }
+
+                    //if player has done their roll and moved, run OnEventTile() function to see if they are on event tile and make event run in accordance  
+                    if (_tileMovementAmount == 0 && !_isMoving && _nextPlayerTurn == false)
+                    {
+
+                        OnEventTile();
+
+                    }
                 }
             }
-        }
+        
     }
 
 
--- Board Game Project/Assets/Scripts/PauseMenu.cs
+++ Board Game Project/Assets/Scripts/PauseMenu.cs
@@ -7,68 +7,37 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
 
-    public bool pauseMenuActive;
+   
     
 
     
     
     Button quitButton;
-    Button resumeButton;
-    Transform pauseMenuBackground;
+   
 
 
-    public void OnResumeButtonClick()
-    {
-        pauseMenuBackground.gameObject.SetActive(false);
-        
-        pauseMenuActive = false;
-        
-    }
+    
 
     public void OnQuitButtonClick()
     {
+       
         SceneManager.LoadScene("MainMenu");
     }
 
-  
+    
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenuActive = false;
-        resumeButton = transform.Find("Pause Menu Background").Find("Resume Button").GetComponent<Button>();
-        quitButton = transform.Find("Pause Menu Background").Find("Quit Button").GetComponent<Button>();
+        
+        quitButton = transform.Find("Quit Button").GetComponent<Button>();
 
 
-        pauseMenuBackground = transform.Find("Pause Menu Background");
-        resumeButton.onClick.AddListener(OnResumeButtonClick);
+       
+       
         quitButton.onClick.AddListener(OnQuitButtonClick); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Debug.Log("Escape was pressed");
-            if(pauseMenuActive == false)
-            {
-                Debug.Log("pause menu is here!");
-                pauseMenuBackground.gameObject.SetActive(true);
-                pauseMenuActive = true;
-                
-            }
-            else if(pauseMenuActive == true)
-            {
-                pauseMenuBackground.gameObject.SetActive(false);
-                pauseMenuActive = false;
-                
-            }
-        }
-        
-
-
-    }
 }
